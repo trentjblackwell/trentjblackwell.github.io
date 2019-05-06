ee5b13d7d72f686ff98e998e78aed3761a93e00f
TrentB <trentjblackwell@gmail.com>
2019-05-06 16:25:07 -0700

    game logic v2/fire mechanics

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+//initialize game boards
+function init() {
+board1 =    [0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0];
+
+board2 =    [0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0];         
+
+    winner = null;
+    turn = 1;
+    render();
+}
+//create ship class
 class Ship {
     constructor(name, length, hitLocs, isSunk, numHits) {
         this.name = name;
@@ -7,6 +28,7 @@ class Ship {
         this.numHits = numHits;
     }
 }
+//create ships
     var ship1 = new Ship("Battleship", 4, ["", "", "", ""], false, 0);
     var ship2 = new Ship("Submarine", 3, ["", "", ""], false, 0);
     var ship3 = new Ship("Destroyer", 2, ["", ""], false, 9);
@@ -14,24 +36,28 @@ class Ship {
     var ship4 = new Ship("Battleship", 4, ["", "", "", ""], false, 0);
     var ship5 = new Ship("Submarine", 3, ["", "", ""], false, 0);
     var ship6 = new Ship("Destroyer", 2, ["", ""], false, 0);
-
-
-///const msgEl = document.getElementById('msgEl');
-  
-function checkForWin() {
+//check for a winner
+  function checkForWin() {
  if (ship1.numHits + ship2.numHits + ship3.numHits === 9) {
-     //var msg = document.getElementsByClassName('msgEl').textContent('Player 2 Wins!!!');
      document.getElementById('msgEl').textContent = 'Player 2 Wins!!!';
      document.getElementById('msgEl').setAttribute('behavior', 'alternate');
     console.log('Player 2 Wins!!!');
  }  
  else if (ship4.numHits + ship5.numHits + ship6.numHits === 9) {
-    
+     document.getElementById('msgEl').textContent = 'Player 1 Wins!!!';
+     document.getElementById('msgEl').setAttribute('behavior', 'alternate');
     console.log('Player 1 Wins!!!');
 }  
 }
+//event listeners for eventual fire() function
+document.getElementById('board1')
+.addEventListener('click', handleClick);
 
-function player1fire(target) {
-    document.getElementById('board2').addEventListener('click', handleClick);
-    
+document.getElementById('board2')
+.addEventListener('click', handleClick);
+
+function handleClick(evt) {
+    const marker = evt.target.getAttribute('id');
+    console.log(marker);
+
 }
